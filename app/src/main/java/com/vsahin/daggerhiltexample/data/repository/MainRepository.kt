3db14670ca801e4a3d9d1668289ce2bc23df27d2
Volkan Sahin <nihas.naklov@gmail.com>
2020-06-12 23:03:02 +0200

    Change Thread with Executor

--- app/src/main/java/com/vsahin/daggerhiltexample/data/repository/MainRepository.kt
+++ app/src/main/java/com/vsahin/daggerhiltexample/data/repository/MainRepository.kt
@@ -3,6 +3,7 @@ package com.vsahin.daggerhiltexample.data.repository
 import androidx.lifecycle.MutableLiveData
 import com.vsahin.daggerhiltexample.data.MyResult
 import dagger.Reusable
+import java.util.concurrent.Executors
 import javax.inject.Inject
 import kotlin.random.Random
 
@@ -11,15 +12,15 @@ class MainRepository @Inject constructor() {
     fun getNumberList(): MutableLiveData<MyResult<List<Int>>> {
         val myResource: MutableLiveData<MyResult<List<Int>>> = MutableLiveData(MyResult.Loading)
 
-        Thread(Runnable {
+        Executors.newSingleThreadExecutor().execute {
             Thread.sleep(SIMULATE_NETWORK_DELAY)
             if (Random.nextInt(2) % 2 == 0) {
                 myResource.postValue(MyResult.Error(Exception()))
-                return@Runnable
+                return@execute
             }
             val numberList = (1..10).map { Random.nextInt(20) }
             myResource.postValue(MyResult.Success(numberList))
-        }).start()
+        }
 
         return myResource
     }
